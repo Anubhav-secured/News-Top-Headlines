2c86e2244fe9cc074c36a769c10356a807690a2e
Anubhav singh <anubhavsinghnit5132@gmail.com>
2022-12-25 13:36:21 +0530

    updated

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+        <div style={{display:"flex" , justifyContent: 'flex-end', position:'absolute', right:'0'}}>
+            <span className="badge rounded-pill bg-danger" style={{left:'85%',zIndex:'1'}}>
+                    {source} 
+            </span>
+
+          </div>
+          <img src={!imageUrl? "https://imgeng.jagran.com/images/2022/dec/Untitled design (31)1671512614734.jpg": imageUrl}className="card-img-top" alt="..."/>
+          <div className="card-body"><h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "Unknown"} On{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
